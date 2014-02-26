7bb0faa766a393a42eec8ae92ff418e79ef659ba
Christian Rondeau <christian.rondeau@mediaclip.ca>
2014-02-26 17:11:48 -0500

    add the suite name to the summary

--- tasks/jasmine.js
+++ tasks/jasmine.js
@@ -241,6 +241,7 @@ module.exports = function(grunt) {
           return error.message;
         });
         summary.push({
+          suite: suites[currentSuite].name,
           name: specMetaData.description,
           errors: specMetaData.failedExpectations.map(function(error){
             return {
@@ -314,7 +315,7 @@ module.exports = function(grunt) {
     function logSummary(tests) {
         grunt.log.writeln('Summary (' + tests.length + ' tests failed)');
         _.forEach(tests, function(test){
-            grunt.log.writeln(chalk.red(symbols['error']) + ' ' + test.name);
+            grunt.log.writeln(chalk.red(symbols['error']) + ' ' + test.suite + ' ' + test.name);
             _.forEach(test.errors, function(error){
               grunt.log.writeln('    ' + chalk.red(error.message));
             });
